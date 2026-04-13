935f68c9da2ccde1ac50e34cf9767d2cd73bbe3a
Lucas Ribeiro <martins7ribeiro@gmail.com>
2026-04-12 22:10:58 -0300

    fix: locked camera zoom fixed - unity version changed

--- Assets/Scripts/V1PlayerMovementLockedCamDetached/MOBACamera.cs
+++ Assets/Scripts/V1PlayerMovementLockedCamDetached/MOBACamera.cs
@@ -7,9 +7,12 @@ public class MOBACamera : MonoBehaviour
     // PUBLIC
     public Transform camTarget;
     public Vector3 offset = new(0f, 10f, -5);
-    public float zoomSpeed = 2f;
-    public float minZoom = 4f;
-    public float maxZoom = 20f;
+    public float zoomSpeedY = 1f;
+    public float zoomSpeedZ = 0.5f;
+    public float minZoomY = 4f;
+    public float maxZoomY = 16f;
+    public float minZoomZ = -8f;
+    public float maxZoomZ = -2f;
 
     void LateUpdate()
     {
@@ -18,9 +21,11 @@ public class MOBACamera : MonoBehaviour
 
         // TODO: fix zoom, currently it doesnt zoom in and out centered correctly
         // Zoom in and out with scroll wheel
-        float scroll = Mouse.current.scroll.ReadValue().y;
-        offset.y -= scroll * zoomSpeed;
-        offset.z += scroll * zoomSpeed;
-        offset.y = Mathf.Clamp(offset.y, minZoom, maxZoom);
+        float scrollY = Mouse.current.scroll.ReadValue().y;
+        // float scrollZ = Mouse.current.scroll.ReadValue().z;
+        offset.y -= scrollY * zoomSpeedY;
+        offset.z += scrollY * zoomSpeedZ;
+        offset.y = Mathf.Clamp(offset.y, minZoomY, maxZoomY);
+        offset.z = Mathf.Clamp(offset.z, minZoomZ, maxZoomZ);
     }
 }
